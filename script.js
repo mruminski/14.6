9d84738ed24afd74ca8b5bce9cd302c4bb102864
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-22 20:03:33 +0200

    Add React Lifecycle methods

--- script.js
+++ script.js
@@ -17,6 +17,38 @@ var Counter = React.createClass({
     });
   },
 
+  // getDefaultProps() {
+  //   console.log('Use this method to set default values for props');
+  // },
+
+  // componentWillMount() {
+  //   console.log('Started before render to DOM');
+  // },
+
+  // componentDidMount() {
+  //   console.log('This method runs immediately after render');
+  // },
+
+  // componentWillReceiveProps() {
+  //   console.log('Invoked only when gets a new props');
+  // },
+
+  // shouldComponentUpdate() {
+  //   console.log('Does it require render again?');
+  // },
+
+  // componentWillUpdate() {
+  //   console.log('Runs if shouldComponentUpdate() return true');
+  // },
+
+  // componentDidUpdate() {
+  //   console.log('This method is analogous to componentDidUpdate()');
+  // },
+
+  // componentWillUnmount() {
+  //   console.log('In this place you can run code before delete component');
+  // },
+
   render: function() {
     return React.createElement(
       'div',
